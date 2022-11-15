79f218e66397a37b0c4cec9f43c6def19f8ef002
Patricia Rodrigues <patricia.soares.o.rodrigues@gmail.com>
2022-11-15 16:28:51 -0300

    Aula 83 - Usando a Estrutura DO/WHILE

---
+++ 5 - JavaScript: Estruturas de Controle/doWhile.js
@@ -0,0 +1,13 @@
+function getInteiroAleatrioEntre(min, max) {
+  const valor = Math.random() * (max - min) + min
+  return Math.floor(valor);
+}
+
+let opcao = -1
+
+do {
+  opcao = getInteiroAleatrioEntre(-1, 10)
+  console.log(`Opção escolhida foi ${opcao}.`);
+} while (opcao != -1)
+
+console.log('Até a próxima!');

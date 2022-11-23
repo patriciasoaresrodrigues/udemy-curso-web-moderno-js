1314342f77215b4d54172046ae774166b81967e3
Patricia Rodrigues <patricia.soares.o.rodrigues@gmail.com>
2022-11-23 16:13:39 -0300

    Exercício 1 e 2

---
+++ 6- JavaScript: Função/Exercícios/1.js
@@ -0,0 +1,8 @@
+function calcularOperacoes(operador1, operador2) {
+  console.log(operador1 + operador2)
+  console.log(operador1 - operador2)
+  console.log(operador1 * operador2)
+  console.log(operador1 / operador2)
+}
+
+calcularOperacoes(2, 2)
---
+++ 6- JavaScript: Função/Exercícios/2.js
@@ -0,0 +1,17 @@
+function classicaTriangulo(lado1, lado2, lado3) {
+  if (lado1 === lado2 && lado2 === lado3) {
+    console.log('Triângulo Equilátero')
+  } else if (lado1 === lado2 || lado2 === lado3 || lado1 === lado3) {
+    console.log('Triângulo Isósceles')
+  } else if (lado1 != lado2 && lado2 != lado3 && lado1 != lado3) {
+    console.log('Triângulo Escaleno')
+  } else {
+    console.log('Valores inválidos!')
+  }
+}
+
+classicaTriangulo(2, 2, 2)
+classicaTriangulo(2, 2, 3)
+classicaTriangulo(2, 3, 4)
+classicaTriangulo(2, 3)
+classicaTriangulo(2, 4, 7, 9)
